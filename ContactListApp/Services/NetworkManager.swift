60dfac44af2ac57499d932a18bb979a80855c4db
vic8tor <timongrinni@gmail.com>
2022-01-23 01:07:03 +0300

    Added data downloading method

--- ContactListApp/Services/NetworkManager.swift
+++ ContactListApp/Services/NetworkManager.swift
@@ -11,26 +11,38 @@ class NetworkManager {
     static let shared = NetworkManager()
 
     private let urlParam = [
-        "results": "\(15)"]
+        "results": "\(15)",
+    ]
     
     private init() {}
     
-    func fetch(_ completion: @escaping (Result<[User], AFError>) -> Void) {
+    func fetchUsers(_ completion: @escaping (Result<[User], AFError>) -> Void) {
         AF.request(URLContacts.randomUserAPI.rawValue, parameters: urlParam)
             .validate()
             .responseJSON { dataResponse in
                 switch dataResponse.result {
                 case .success(let value):
                     let users = User.getRandomUser(from: value)
-                    print(users)
                     completion(.success(users))
                 case .failure(let error):
                     completion(.failure(error))
                 }
             }
+        }
+
+    func fetchData(from url: String, _ completion: @escaping (Result<Data, AFError>) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
     }
-
-
+    
 
 }
 
