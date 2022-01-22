6e3107e5d53665f76c07ddfb0c964c27078f2d2c
vic8tor <timongrinni@gmail.com>
2022-01-22 20:42:17 +0300

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Victor on 22.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
